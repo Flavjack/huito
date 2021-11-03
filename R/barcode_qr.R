1a5130dab8557b232189ece5e9c20c78e85b1875
Flavjack <flavjack@gmail.com>
2021-11-03 01:55:25 +0100

    fix code qr

--- R/barcode_qr.R
+++ R/barcode_qr.R
@@ -5,39 +5,34 @@
 #' @param text text to convert to QR bar code
 #' @param color Bar code color
 #' @param alpha Intensity of the bar code color
+#' @param ecl Error correction level (percentage). "L" (7), "M" (15), "Q" (25)
+#'   and "H" (30). Defaults to "H"
 #'
 #' @return plot
 #'
 #' @export
-#'
-#' @examples
-#'
-#' \dontrun{
-#'
-#' barcode_qr("inkaverse")
-#'
-#' }
-#'
+#' 
 
 barcode_qr <- function(text
-                          , color = "black"
-                          , alpha = 1
+                       , color = "black"
+                       , alpha = 1
+                       , ecl = "H"
                        ) {
-
-  qrmt <- qrcode::qrcode_gen(text
-                             , plotQRcode = F
-                             , dataOutput = T) %>%
-    as.data.frame()
-
+  
+  qrmt <- qrcode::qr_code(x = text, ecl = ecl) %>% 
+    as.data.frame() %>% 
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric))
+  
   dt <- qrmt %>%
     dplyr::mutate(id = rownames(.)) %>%
     tidyr::pivot_longer(!.data$id, names_to = "key", values_to = "value") %>%
     dplyr::mutate(dplyr::across(c(.data$key, .data$id)
-                                , ~ forcats::as_factor(.)))
-
+                              , ~ forcats::as_factor(.)))
+  
   ggplot(dt, aes(x = .data$id, y = .data$key)) +
     geom_tile(aes(fill = .data$value), alpha = alpha) +
     scale_fill_gradient(low = "white", high = color) +
     theme_void() +
     theme(legend.position = 'none')
+  
 }
