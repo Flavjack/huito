537cafa1e3ea7556520c33c1fcb8467279ac34e4
Flavjack <flavjack@gmail.com>
2021-11-03 02:49:08 +0100

    qr fix margen

--- R/barcode_qr.R
+++ R/barcode_qr.R
@@ -11,6 +11,16 @@
 #' @return plot
 #'
 #' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' library(huito)
+#' 
+#' barcode_qr("LIMA-2021-11-03_15_3_4")
+#'
+#' }
 #' 
 
 barcode_qr <- function(text
@@ -19,17 +29,27 @@ barcode_qr <- function(text
                        , ecl = "H"
                        ) {
   
+  if(FALSE) {
+    
+    text = "LIMA-2021-11-03_15_3_4"
+    
+  }
+  
   qrmt <- qrcode::qr_code(x = text, ecl = ecl) %>% 
     as.data.frame() %>% 
-    dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric))
-  
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric)) %>% 
+    dplyr::select(!c(1:2)) %>% 
+    dplyr::select(!c((ncol(.)-1):ncol(.))) %>% 
+    utils::head(-2) %>% 
+    utils::tail(-2)
+
   dt <- qrmt %>%
     dplyr::mutate(id = rownames(.)) %>%
     tidyr::pivot_longer(!.data$id, names_to = "key", values_to = "value") %>%
     dplyr::mutate(dplyr::across(c(.data$key, .data$id)
                               , ~ forcats::as_factor(.)))
-  
-  ggplot(dt, aes(x = .data$id, y = .data$key)) +
+  dt %>% 
+    ggplot(aes(x = .data$id, y = .data$key)) +
     geom_tile(aes(fill = .data$value), alpha = alpha) +
     scale_fill_gradient(low = "white", high = color) +
     theme_void() +
