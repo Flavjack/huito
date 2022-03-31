7172b30884a17bccabd7a3b938cb821f6ae9ff54
Flavjack <flavjack@gmail.com>
2022-03-31 13:01:41 -0500

    webpage

---
+++ docs/logo.R
@@ -0,0 +1,272 @@
+# -------------------------------------------------------------------------
+# hutio ---------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+library(huito)
+
+#> https://github.com/terinjokes/StickersStandard
+
+font <- "Permanent Marker"
+
+huito_fonts(font)
+
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+                      ) %>% 
+  include_image(value = "pkgdown/favicon/img/shipibo.png"
+                , size = c(7, 7)
+                , position = c(2.55, 2.52)
+                ) %>%
+  include_text(value = "H", size = 45 , position = c(1.15, 2.7), color = "#00a85a", font) %>%
+  include_text(value = "u", size = 45 , position = c(2.07, 2.7), color = "#f58735", font) %>%
+  include_text(value = "i", size = 45 , position = c(2.73, 2.7), color = "#4774b8", font) %>%
+  include_image(value = "pkgdown/favicon/img/huito_fruit.png"
+                , size = c(1.35, 1.35) 
+                , position = c(4.03, 2.65)
+                ) %>%
+  include_text(value = "t", size = 45 , position = c(3.33, 2.7), color = "#a9518b", font) %>%
+  include_shape(size = 4.1
+                , border_width = 3
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_color = "blue"
+                , panel_size = 5.08
+                ) %>%
+  include_text(value = "inkaverse.com"
+               , size = 6
+               , position = c(3.6, 0.75)
+               , angle = 30
+               , color = "white"
+               , font
+               ) %>%
+  label_print(filename = "pkgdown/favicon/img/huito"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 300
+              , mode = "s"
+              )
+
+# transparent -------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label %>% 
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent("blue") %>% 
+  image_write("man/figures/logo.png")
+
+
+# -------------------------------------------------------------------------
+# Flavio Lozano Isla ------------------------------------------------------
+# -------------------------------------------------------------------------
+
+library(huito)
+
+font <- "Fredericka the Great"
+
+huito_fonts(font)
+
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+                      , background = "white"
+                      ) %>% 
+  include_image(value = "pkgdown/favicon/img/shipibo.png"
+                , size = c(7, 7)
+                , position = c(2.55, 2.52)
+                , opts = 'image_transparent("white")'
+                ) %>%
+  include_shape(size = 4.1
+                , border_width = 2.5
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_size = 5.08
+                , panel_color = "blue" 
+                ) %>%
+  include_shape(size = 3.7
+                , border_width = 0.1
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_size = 5.08
+                , panel_color = NA
+                , background = "white"
+                ) %>%
+  include_text(value = "Flavio"
+               , position = "2.65*3.4"
+               , size = 27
+               , font = font
+               ) %>% 
+  include_text(value = "Lozano"
+               , position = "2.55*2.55"
+               , size = 27
+               , font = font
+               ) %>% 
+  include_text(value = "Isla"
+               , position = "2.6*1.65"
+               , size = 27
+               , font = font
+               ) %>%
+  include_text(value = "lozanoisla.com"
+               , size = 5
+               , position = c(3.6, 0.75)
+               , angle = 30
+               , font = font, color = "white") %>%
+  label_print(filename = "pkgdown/favicon/img/flozano"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 300
+              , mode = "c"
+              )
+
+# transparent -------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label %>% 
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent("blue") %>%  
+  image_write("pkgdown/favicon/img/flozano.png")
+
+
+
+# -------------------------------------------------------------------------
+# Sandy -------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+library(huito)
+
+font <- "Fredericka the Great"
+
+huito_fonts(font)
+
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+                      , background = "pink"
+                      ) %>% 
+  include_image(value = "pkgdown/favicon/img/shipibo.png"
+                , size = c(7, 7)
+                , position = c(2.55, 2.52)
+                , opts = 'image_transparent("white")'
+                ) %>%
+  include_shape(size = 4.1
+                , border_width = 2.5
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_size = 5.08
+                , panel_color = "red"
+                ) %>%
+  include_shape(size = 3.7
+                , border_width = 0.1
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_size = 5.08
+                , panel_color = "transparent"
+                , background = "white"
+                ) %>%
+  include_text(value = "Sandy"
+               , position = "2.55*3.4"
+               , size = 20
+               , font = font
+               ) %>% 
+  include_text(value = "Ordoñez"
+               , position = "2.55*2.65"
+               , size = 19
+               , font = font
+               ) %>% 
+  include_text(value = "Del Aguila"
+               , position = "2.6*1.9"
+               , size = 19
+               , font = font
+               ) %>%
+  label_print(filename = "pkgdown/favicon/img/sandy"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 300
+              , mode = "s"
+              )
+
+# transparent -------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label %>% 
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent("red") %>% 
+  image_write("pkgdown/favicon/img/sandy.png")
+
+
+# -------------------------------------------------------------------------
+# Omar Estefano -----------------------------------------------------------
+# -------------------------------------------------------------------------
+
+library(huito)
+
+font <- "Fredericka the Great"
+
+huito_fonts(font)
+
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+) %>% 
+  include_image(value = "pkgdown/favicon/img/shipibo.png"
+                , size = c(7, 7)
+                , position = c(2.55, 2.52)
+  ) %>%
+  include_shape(size = 4.1
+                , border_width = 2.5
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_color = "#abc621"
+                , panel_size = 5.08
+  ) %>%
+  include_shape(size = 3.7
+                , border_width = 0.1
+                , border_color = "black"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_size = 5.08
+                , panel_color = NA
+                , background = "white"
+  ) %>%
+  include_text(value = "Omar"
+               , position = "2.55*3.1"
+               , size = 28
+               , font = font
+  ) %>% 
+  include_text(value = "Estefano"
+               , position = "2.55*2"
+               , size = 22
+               , font = font
+  ) %>% 
+  label_print(filename = "pkgdown/favicon/img/omar"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 300
+              , mode = "c"
+              )
+
+# transparent -------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label %>% 
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent("#abc621") %>% 
+  image_write("pkgdown/favicon/img/omar.png")
---
+++ docs/phenotyping.R
@@ -0,0 +1,117 @@
+library(huito)
+source("http://inkaverse.com/setup.r")
+
+# import scale ------------------------------------------------------------
+
+scale <- image_read_pdf("pkgdown/favicon/img/scale.pdf", density = 600) %>% 
+  # image_rotate(-90) %>% 
+  image_write("pkgdown/favicon/img/scale.png")
+
+# phenotyping-blue --------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label <- label_layout(size = c(100, 100)
+                      , border_color = "black"
+                      , border_width = 10
+                      , background = "#0099FF"
+                      ) 
+
+label %>% 
+  label_print(mode = "c"
+              , paper = c(110, 110)
+              , filename = "pkgdown/favicon/img/phenotyping_blue")
+
+# phenotyping-bw ----------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label <- label_layout(size = c(100, 100)
+                      , border_color = "black"
+                      , border_width = 10
+                      , background = NA
+                      ) 
+
+label %>% 
+  label_print(mode = "c"
+              , paper = c(110, 110)
+              , filename = "pkgdown/favicon/img/phenotyping_bw")
+
+# scales ------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+label <- label_layout(size = c(12, 6)
+                      , border_color = NA
+                      , border_width = 0
+                      ) %>% 
+  include_image(value = scale
+                , size = c(11, 5)
+                , position = c(6, 3)
+                ) 
+
+label %>% 
+  label_print(mode = "c"
+              , paper = c(100, 40)
+              , filename = "pkgdown/favicon/img/scale-print"
+              , nlabels = 48
+              )
+
+# labels ------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+url <- "https://docs.google.com/spreadsheets/d/1jcXXUzoMsXEMeccfs2AeQLDvC_3_B9QjzeFvnx6AT3Y/edit#gid=1266812467"
+
+gs <- url %>% 
+  as_sheets_id()
+
+fb <- gs %>% 
+  range_read("fb") 
+
+huito_fonts("Rubik")
+
+label <- fb %>% 
+  label_layout(size = c(12, 5)
+               , border_color = "red"
+               , border_width = 2
+               ) %>%
+  include_barcode(
+    value = "barcode"
+    , size = c(4.5, 4.5)
+    , position = c(9, 2.5)
+    ) %>% 
+  include_text(value = "id"
+               , position = c(11.5, 2.5)
+               , angle = 90
+               , size = 20
+               , font = "Rubik"
+               ) %>% 
+  include_image(
+    value = "man/figures/logo.png"
+    , size = c(3, 3)
+    , position = c(4, 2.3)
+    ) %>% 
+  include_text(value = "PhenomQuinoa"
+               , position = c(4, 4.5)
+               , size = 20
+               , color = "brown"
+               , font = 'Rubik'
+               ) %>% 
+  include_text(value = "barcode"
+               , position = c(0.5, 2.5)
+               , angle = 90
+               , size = 15
+               , font = "Rubik"
+               ) %>% 
+  include_text(value = "12x5 cm"
+               , position = c(4, 0.3)
+               , size = 10
+               , color = "brown"
+               , font = 'Rubik'
+               )
+
+label %>% label_print()
+  
+label %>% 
+  label_print(mode = "c"
+              , filename = "pkgdown/favicon/labels"
+              , paper = c(100, 100)
+              )
+
