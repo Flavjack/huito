91942c5a9656b82ec562f6b9c738a121696472c0
Flavio <flavjack@gmail.com>
2022-05-29 11:43:38 -0500

    updt web page

--- docs/logo.R
+++ docs/logo.R
@@ -24,13 +24,11 @@ label <- label_layout(size = c(5.08, 5.08)
                 , position = c(4.03, 2.65)
                 ) %>%
   include_text(value = "t", size = 45 , position = c(3.33, 2.7), color = "#a9518b", font) %>%
-  include_shape(size = 4.1
+  include_shape(size = 5.08
                 , border_width = 3
                 , border_color = "black"
-                , margin = -0.8
                 , position = c(2.54, 2.54)
                 , panel_color = "blue"
-                , panel_size = 5.08
                 ) %>%
   include_text(value = "inkaverse.com"
                , size = 6
@@ -56,8 +54,11 @@ stiker <- logo %>%
   image_read_pdf()  %>% 
   image_crop(geometry = "600x600+40") %>% 
   image_crop(geometry = "560x600-40") %>% 
-  image_transparent("blue") %>% 
-  image_write("man/figures/logo.png")
+  image_transparent("blue") %>%
+  image_write("pkgdown/favicon/img/huito.png")
+
+file.copy("pkgdown/favicon/img/huito.png", "man/figures/logo.png"
+          , overwrite = T)
 
 # -------------------------------------------------------------------------
 # Flavio Lozano Isla ------------------------------------------------------
@@ -65,7 +66,7 @@ stiker <- logo %>%
 
 library(huito)
 
-font <- "Fredericka the Great"
+font <- c("Fredericka the Great", "Permanent Marker")
 
 huito_fonts(font)
 
@@ -79,43 +80,40 @@ label <- label_layout(size = c(5.08, 5.08)
                 , position = c(2.55, 2.52)
                 , opts = 'image_transparent("white")'
                 ) %>%
-  include_shape(size = 4.1
-                , border_width = 2.5
+  include_shape(size = 5.08
+                , border_width = 2.2
                 , border_color = "black"
-                , margin = -0.8
                 , position = c(2.54, 2.54)
-                , panel_size = 5.08
                 , panel_color = "blue" 
                 ) %>%
-  include_shape(size = 3.7
+  include_shape(size = 4
                 , border_width = 0.1
                 , border_color = "black"
-                , margin = -0.8
                 , position = c(2.54, 2.54)
-                , panel_size = 5.08
                 , panel_color = NA
                 , background = "white"
                 ) %>%
   include_text(value = "Flavio"
                , position = "2.65*3.4"
                , size = 27
-               , font = font
+               , font = font[1]
                ) %>% 
   include_text(value = "Lozano"
                , position = "2.55*2.55"
                , size = 27
-               , font = font
+               , font = font[1]
                ) %>% 
   include_text(value = "Isla"
                , position = "2.6*1.65"
                , size = 27
-               , font = font
+               , font = font[1]
                ) %>%
   include_text(value = "lozanoisla.com"
                , size = 5
-               , position = c(3.6, 0.75)
+               , position = c(3.6, 0.72)
                , angle = 30
-               , font = font, color = "white") 
+               , font = font[2]
+               , color = "white") 
 
 label %>% label_print()
   
